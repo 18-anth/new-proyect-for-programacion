6e19fd732b9447c839100f82a2087a3c67440bb5
Anthony Córdova <anthonycordova@gmail.com>
2020-10-05 19:26:35 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,227 @@
+heroes =[
+    {
+        'id': 'dc-arrow',
+        'superhero':'Green Arrow', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Oliver Queen',
+        'first_appearance':'More Fun Comics #73',
+        'characters':'Oliver Queen',
+        'information':'Flecha Verde es un héroe vigilante que combate al crimen utilizando la arquería,'
+                        'artes marciales y la tecnología.'
+                        'En su identidad secreta es Oliver Queen y vive en Ciudad Estrella como un multimillonario y playboy.'
+    },
+    {
+        'id': 'dc-batman',
+        'superhero':'Batman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Bruce Wayne',
+        'first_appearance':'Detective Comics #27',
+        'characters':'Bruce Wayne',
+        'information':'Batman es un superhéroe que nació para el cómic un año antes que termine la década del cuarenta del siglo pasado, sin embargo,' 
+                    'y como ningún otro superhéroe, logró trascender las fronteras del mismísimo papel, donde todos los superhéroes tuvieron sus inicios y dieron sus primeros pasos,' 
+                    'y recaló en el cine y la tevé con un éxito formidable y que iría in crescendo a través del tiempo y las diferentes adaptaciones de la historia original.'
+    },
+    {
+        'id': 'dc-black',
+        'superhero':'Black Canary', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dinah Drake',
+        'first_appearance':'Flash Comics #86',
+        'characters':'Dinah Drake, Dinah Lance',
+        'information':'Es un vigilante de superhéroes que lucha contra el crimen usando artes marciales y'
+                        'un ataque de gritos sónicos. Ella es una de las mejores luchadoras en el Universo DC,y'
+                        'también una fuerte mente táctica. Su traje es un atractivo traje negro con medias de red y pelo rubio,' 
+                        'y por lo general se la ve montando en su motocicleta.'
+    },
+    {
+        'id': 'dc-blue',
+        'superhero':'Blue Beetle', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dan Garret',
+        'first_appearance':'Mystery Men Comics #1',
+        'characters':'Dan Garret, Ted Kord, Jaime Reyes',
+        'information':'Es el nombre heredado de varios superhéroes con diferentes habilidades.'
+                        'Dan Garrett es la versión original, un arqueólogo que descubre y utiliza el Escarabajo Azul Escarabajo para luchar contra el crimen.'
+                        'Su sucesor fue Ted Kord,tomando el manto de su mentor usando artes marciales y artilugios cuando no pudo desbloquear el poder del escarabajo,'
+                        'llegando a ser miembro de justice League International, Living Assault Weapons y Super Buddies.'
+    },
+    {
+        'id': 'dc-green',
+        'superhero':'Green Lantern', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Alan Scott',
+        'first_appearance':'All-American Comics #16',
+        'characters':'Alan Scott, Hal Jordan, Guy Gardner, '
+                    'John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz',
+        'information':'Linterna Verde es el alter ego de muchos superhéroes' 
+                    'ficticios que han aparcado en las diversas producciones de la mega productora de comic DC Comics.'
+    },
+    {
+        'id': 'dc-martian',
+        'superhero':'Martian Manhunter', 
+        'publisher':'DC Comics', 
+        'alter_ego':'J\'onn J\'onzz',
+        'first_appearance':'Detective Comics #225',
+        'characters':'Martian Manhunter',
+        'information':'El Detective Marciano (Jonn, Jonzz) es un superhéroe de la compañía DC Comics.'
+                        'Apareció por primera vez en Detective Comics Nº 225 (noviembre de 1955), en la historia corta "El extraño experimento del Dr.'
+                        'Erdel" escrita por Joseph Samachson e ilustrada por Joe Certa'
+    },
+    {
+        'id': 'dc-robin',
+        'superhero':'Robin/Nightwing', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dick Grayson',
+        'first_appearance':'Detective Comics #38',
+        'characters':'Dick Grayson',
+        'information':'Después de haber pasado su gestación en un laboratorio, Damian fue criado por su madre hasta los diez años de edad,'
+                        'momento en el que lo dejó al cuidado de su padre, que no había sido consciente de la existencia de su hijo hasta ese momento en el tiempo. '
+                        'Damian es violento y auto-importante, y fue entrenado por la Liga de Asesinos,aprendiendo a matar a una edad temprana.'
+    },
+    {
+        'id': 'dc-flash',
+        'superhero':'Flash', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Jay Garrick',
+        'first_appearance':'Flash Comics #1',
+        'characters':'Jay Garrick, Barry Allen, Wally West, Bart Allen',
+        'information':'The Flash es el nombre de un superhéroe creado por la empresa DC Comics en los años cuarenta del siglo pasado,'
+                        'en tanto, su denominación tiene mucho que ver con su superpoder esencial y preponderante que es la rapidez, porque como sabemos, '
+                        'una de las referencias de esta palabra en el idioma inglés designa aquello que es rápido.'
+    },
+    {
+        'id': 'dc-superman',
+        'superhero':'Superman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Kal-El',
+        'first_appearance':'Action Comics #1',
+        'characters':'Kal-El',
+        'information':'Superman es un superhéroe de ficción que nació en los comics estadounidenses allá por la década del treinta del siglo pasado,' 
+                    'aunque, pronto, a fuerza de la magnífica repercusión y extensión del personaje se convirtió en un ícono cultural al cual se echa mano cada vez que es necesario expresar o manifestar la fuerza,'
+                    'la resolución y presencia de un hombre… porque Superman es justamente eso un “superhombre”.'
+    },
+    {
+        'id': 'dc-wonder',
+        'superhero':'Wonder Woman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Princess Diana',
+        'first_appearance':'All Star Comics #8',
+        'characters':'Princess Diana',
+        'information':'La Mujer Maravilla (Wonder Woman en inglés) es una superheroína de DC Comics que aparece en historietas,'
+                        'dibujos animados y películas. creada por el psicólogo estadounidense William Moulton Marston bajo el seudónimo de Charles Moulton. Marston tomó como modelos a Elizabeth (Sadie) Holloway Marston, su esposa,'
+                        'y a Olive Byrne, con quien vivían en una relación de a tres.'
+    },
+    {
+        'id': 'marvel-captain',
+        'superhero':'Captain America', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Steve Rogers',
+        'first_appearance':'Captain America Comics #1',
+        'characters':'Steve Rogers',
+        'information':'El Capitán América es uno de los superhéroes más legendarios de la editorial Marvel Comics.'
+                        'Los autores Jack Kirby y Joe Simon lo crearon cuando corría el año 1941,'
+                        'un año marcado a fuego por la entrada de los Estados Unidos a la disputa de la Segunda Guerra Mundial.'
+    },
+    {
+        'id': 'marvel-cyclops',
+        'superhero':'Cyclops', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Scott Summers',
+        'first_appearance':'X-Men #1',
+        'characters':'Scott Summers',
+        'information':'Cíclope (Scott Summers) es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics y es miembro fundador de los X-Men.'
+                    'Creado por el escritor Stan Lee y el artista Jack Kirby, el personaje apareció por primera vez en el cómic The X-Men de 1963, en la Edad de Plata de los cómics.'
+                    'Cíclope es un miembro de una subespecie de humanos conocidos como mutantes, que nacen con habilidades sobrehumanas. Cíclope puede emitir potentes rayos de energía de sus ojos. '
+    },
+    {
+        'id': 'marvel-daredevil',
+        'superhero':'Daredevil', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Matthew Michael Murdock',
+        'first_appearance':'Daredevil #1',
+        'characters':'Matthew Michael Murdock',
+        'information':'Los orígenes de Daredevil provienen de un accidente infantil que le dio habilidades especiales.'
+                        'Mientras crece en el históricamente arenoso o crimen-clase de trabajo irlandesa-estadounidense vecindario de Hells Kitchen en la ciudad de Nueva York,'
+                        'Matt Murdock es cegado por una sustancia radiactiva que cae de un camión fuera de control después de empujar a un hombre fuera del camino del vehículo que se aproxima.'
+                        'Aunque ya no puede ver, su exposición al material radiactivo aumenta sus sentidos restantes más allá de la capacidad humana normal, y le da un "sentido del radar". Su padre, un boxeador llamado Jack Murdock,es un hombre soltero criando a su hijo ahora ciego, que a pesar de su dura educación,'
+                        'ama incondicionalmente a su hijo y trata de enseñarle a formar una vida mejor para sí mismo. '
+    },
+    {
+        'id': 'marvel-hawkeye',
+        'superhero':'Hawkeye', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Clinton Francis Barton',
+        'first_appearance':'Tales of Suspense #57',
+        'characters':'Clinton Francis Barton',
+        'information':'Un tirador experto y luchador, Clint Barton pone su talento a buen uso trabajando para S.H.I.E.L.D. como agente especial. '
+                        'El arquero conocido como Ojo de Halcón también cuenta con una fuerte brújula moral que a veces lo desvía de sus órdenes directas.'
+    },
+    {
+        'id': 'marvel-hulk',
+        'superhero':'Hulk', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Bruce Banner',
+        'first_appearance':'The Incredible Hulk #1',
+        'characters':'Bruce Banner',
+        'information':'En la colección Biblioteca Marvel: Hulk se publicó de forma cronológica toda la etapa clásica de Hulk, desde su creación en mayo de 1962 en The incredible Hulk vol.1 #1 hasta The Incredible Hulk vol.1 #266 (diciembre de 1981),'
+                    'incluyendo todos los Annuals y los crossovers con otras series, así como la larga etapa en que compartió publicación con el Hombre Gigante y con Namor en Tales to Astonish y los nueve números del magazine The Rampaging Hulk.'
+                    'Los veinte primeros números de la colección Biblioteca Marvel: Hulk fueron editados por Forum y posteriormente Panini, respetando el formato, concluyó la colección editando desde el número 21 al 36.'
+    },
+    {
+        'id': 'marvel-iron',
+        'superhero':'Iron Man', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Tony Stark',
+        'first_appearance':'Tales of Suspense #39',
+        'characters':'Tony Stark',
+        'information':'El genial inventor multimillonario, industrial y CEO de Industrias Stark Tony Stark fue herido fatalmente en una zona de guerra en Afganistán poco antes de ser secuestrado por terroristas.'
+                        'Stark construyó un traje blindado para escapar del cautiverio, que se duplicó como un dispositivo para mantener su corazón latiendo manteniéndolo con vida.'
+                        'Una vez que regresó a los Estados Unidos, Stark se reinventó como el superhéroe revestido de armadura Iron Man.'
+    },
+    {
+        'id': 'marvel-silver',
+        'superhero':'silver surfer', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Norrin Radd',
+        'first_appearance':'The Fantastic Four #48',
+        'characters':'Norrin Radd',
+        'information':'es un personaje ficticio de cómics creado por Jack Kirby en 1966 y publicado por Marvel Comics.'
+                        'Hizo su debut en The Fantastic Four (vol. 1[6]) en la Trilogía Galactus de1966,'
+                        'cuya respuesta pública positiva llevó a su propia serie en 1968 diseñada por John Buscema,quien, aunque efímero, ganó el prestigioso premio Alleyen 1968 y 1969.'
+                        'El sitio web de IGN clasificó a Silver Surfer en el puesto 41 en la lista de los 100 héroes más grandes en la historia de los cómics. '
+    },
+    {
+        'id': 'marvel-spider',
+        'superhero':'Spider Man', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Peter Parker',
+        'first_appearance':'Amazing Fantasy #15',
+        'characters':'Peter Parker',
+        'information':'Marvels Spider-Man, o simplemente Spider-Man, es una serie de televisión animada estadounidense basada en el personaje de Marvel Comics del mismo nombre.'
+                        'Se trata de un reinicio de la serie animada Ultimate Spider-Man. El desarrollo de la serie fue confirmada en octubre de 2016, con planes de presentar a Venom, Miles Morales y Spider-Gwen.'
+                        'La primera temporada fue precedida por una serie de cortos denominados Marvel Spider-Man Origin que se emitieron en el canal de Disney XD en Youtube.'
+                        'La primera temporada de la serie fue estrenada oficialmente el 19 de agosto de 2017 en Estados Unidos.'
+    },
+    {
+        'id': 'marvel-thor',
+        'superhero':'Thor', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Thor Odinson',
+        'first_appearance':'Journey into Myster #83',
+        'characters':'Thor Odinson',
+        'information':'Saltando de las leyendas de la mitología nórdica, el Dios Asgardiano del Trueno, Thor Odinson,es el hijo de Odín el Padre y Jord,'
+                    'la Diosa Mayor y el espíritu de la Tierra. Con frecuencia se para junto a los Vengadores en la defensa del pueblo de Midgard (Tierra), '
+                    'a menudo contra los planes de su hermano adoptivo Loki.'
+                    'Anteriormente vivía en una recreación de Asgard en Oklahoma,usando la forma mortal de Donald Blake para proporcionar un puente entre su pueblo y los mortales que los rodeaban. '
+    },
+    {
+        'id': 'marvel-wolverine',
+        'superhero':'Wolverine', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'James Howlett',
+        'first_appearance':'The Incredible Hulk #180',
+        'characters':'James Howlett', 
+        'information':'Del desierto septentrional de Canadá saluda a uno de los miembros más ásperos, irascibles,'
+                        'totalmente cínicos y melancólicos de los X-Men para adornar al equipo con su presencia.'
+    }
+]
